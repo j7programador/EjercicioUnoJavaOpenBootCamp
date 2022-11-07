6cc8ea5469913ea569c0faac5db04f86face9f4f
j7programador <j7programador@gmail.com>
2022-11-07 15:46:30 -0500

    Ejercicio Uno openBootCamp Java básico

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String cadenaString = "Hello world!";
+        int numero = 5;
+        double numeroDouble = 5.5;
+        long numeroLong = 2819834709L;
+        boolean falsoVerdadero = true;
+
+        System.out.println(cadenaString);
+        System.out.println(numero);
+        System.out.println(numeroDouble);
+        System.out.println(numeroLong);
+        System.out.println(falsoVerdadero);
+
+    }
+
+}
